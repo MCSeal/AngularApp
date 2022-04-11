ae5e1da2d290d35eefbf1de6fe6c4564f3d0b639
MCSeal <mzeal93@gmail.com>
2022-04-11 18:00:12 -0400

    working through

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,17 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'doctors appointment',
+    day: 'may 5 at 2pm',
+    reminder: true,
+  },
+
+  {
+    id: 2,
+    text: 'meeting at school',
+    day: 'may 6 at 1pm',
+    reminder: false,
+  },
+];
